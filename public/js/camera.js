a8a1799da06bbce6a752b8d9632a3b8dc9e0e440
= <vung@ucsd.edu>
2021-02-25 11:18:10 -0800

    finished lab

--- public/js/camera.js
+++ public/js/camera.js
@@ -3,47 +3,44 @@ function initCamera() {
   // capture($('<Your code here>'),
   //        $('<Your code here>'),
   //        $('<Your code here>'));
-
   // For Reference:
-  // capture($('#camera-video'),
-  //       $('#camera-canvas'),
-  //       $('#camera-button'));
+  capture($("#camera-video"), $("#camera-canvas"), $("#camera-button"));
 }
 
 function capture(video, canvas, snapshotButton) {
   //Adopted from https://dev.opera.com/articles/media-capture-in-mobile-browsers/
   //Setup navigator for all versions of browsers.
-  navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-              navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  var ctx = canvas[0].getContext('2d');
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+  var ctx = canvas[0].getContext("2d");
 
-  var successCallback = function(mediaStream) {
+  var successCallback = function (mediaStream) {
     //The success callback function. On user click of snapshot button,
     //draw the image on the canvas.
     // video.attr('src', window.URL.createObjectURL(mediaStream));
-    var vid = document.querySelector('#camera-video');
+    var vid = document.querySelector("#camera-video");
     vid.srcObject = mediaStream;
-    snapshotButton.click(function(e) {
-        console.log("Taking photo");
-        //Calculate dimension of photo from the video element.
-        var width = video.width();
-        var height = video.height();
-        
-        canvas.attr('width', width);
-        canvas.attr('height', height);
-        ctx.drawImage(video[0], 0, 0, width, height);
+    snapshotButton.click(function (e) {
+      console.log("Taking photo");
+      //Calculate dimension of photo from the video element.
+      var width = video.width();
+      var height = video.height();
+
+      canvas.attr("width", width);
+      canvas.attr("height", height);
+      ctx.drawImage(video[0], 0, 0, width, height);
     });
   };
 
-  var errorCallback = function() {
+  var errorCallback = function () {
     //The error callback function. If getUserMedia errored, print that
     //we failed.
-    console.log('Capture failed');
+    console.log("Capture failed");
   };
 
   //Register the success and error callbacks with getUserMedia.
-  navigator.getUserMedia({ 'video': true },
-      successCallback, errorCallback);
-
-};
-
+  navigator.getUserMedia({ video: true }, successCallback, errorCallback);
+}
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+  // add your code here
+  L.mapquest.key = "MxHCwBYD7euZiGvIUD4PSxuGbNGebocK";
+
+  var map = L.mapquest.map("map", {
+    center: [32.88, -117.236],
+    layers: L.mapquest.tileLayer("map"),
+    zoom: 12,
+    zoomControl: false,
+  });
+
+  L.marker([32.88, -117.236]).addTo(map);
 }
